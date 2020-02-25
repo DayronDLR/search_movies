18db30ca5fcacb06c0d2a2d373a3e86be13f15d1
DayronDLR <dlr.dayron@gmail.com>
2020-02-24 19:28:09 -0500

    primera iteración

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import 'bulma/css/bulma.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Title} from './components/Title';
+import {SearchForm} from './components/SearchForm';
+
+class App extends Component {
+  state = {movies: []}
+  
+  _handleResults = (movies) => {
+    this.setState({movies})
+  }
+
+  _renderMovies = () => {
+    let {movies} = this.state
+    return movies.map(movie => {
+      return <p key={movie.imdbID}>{movie.Title}</p>
+    })
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <Title>Search movies</Title>
+        <div className="SearchForm">
+          <SearchForm onResults={this._handleResults}></SearchForm>
+        </div>
+        {
+          this.state.movies.length === 0 ? 
+          <p>Sin resultados</p> :
+          this._renderMovies()
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/SearchForm.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+
+const API_KEY = '4287ad07'
+
+export class SearchForm extends Component {
+    state = {
+        inputMovie: ''
+    }
+
+    _handleChange = (oEvent) => {
+        this.setState({inputMovie: oEvent.target.value})
+    }
+
+    _handleSubmit = (oEvent) => {
+        oEvent.preventDefault();
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${this.state.inputMovie}`)
+            .then(res => res.json())
+            .then(oData => {
+                var {Search, totalResults} = oData
+                this.props.onResults(Search)
+            })
+    }
+
+    render () {
+        return (
+            <form onSubmit={this._handleSubmit}>
+                <div className="field has-addons">
+                    <div className="control">
+                        <input 
+                            className="input"
+                            onChange={this._handleChange}
+                            placeholder="Find a repository" 
+                            type="text" />
+                    </div>
+                    <div className="control">
+                        <button className="button is-info">
+                        Search
+                        </button>
+                    </div>
+                </div>
+            </form>
+        )
+    }
+}
